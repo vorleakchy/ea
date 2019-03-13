65b99228d9b3c2e7c43f818c50005f95f849f741
Vorleak Chy <vorleak.chy@gmail.com>
2019-03-13 17:52:00 -0500

    Add lab12 solution

--- Lab_12_REST/src/main/java/edu/mum/controller/ItemController.java
+++ Lab_12_REST/src/main/java/edu/mum/controller/ItemController.java
@@ -1,62 +1,42 @@
 package edu.mum.controller;
 
-import javax.validation.Valid;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import edu.mum.domain.Item;
 import edu.mum.service.ItemService;
 
-@Controller
-@RequestMapping("/items")
+@RestController
+@RequestMapping({"/items"})
 public class ItemController {
 	
 	@Autowired
-	private ItemService itemService;
+	private ItemService  itemService;
+
+	@RequestMapping
+	public List<Item>  findAll( ) {
+		List<Item> itemList = itemService.findAll();
+		return  itemList;
+	}
+	
+  	@RequestMapping("/{id}")
+	public Item getItemById(@PathVariable("id") Long id) {
+		return   itemService.findOne(id);
  
- 	@RequestMapping({"","/all"})
-	public String list(Model model) {
-		model.addAttribute("items", itemService.findAll());
-		return "items";
-	}
-	
- 	@RequestMapping("/item")
-	public String getItemById(Model model, @RequestParam("id") Long id) {
-
-		Item item = itemService.findOne(id);
-		model.addAttribute("item", item);
-		return "item";
-	}
-
-	
-	@RequestMapping(value = "/add", method = RequestMethod.GET)
-	public String getAddNewItemForm(@ModelAttribute("newItem") Item newItem) {
-	   return "addItem";
 	}
 	   
-	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String processAddNewItemForm(@ModelAttribute("newItem") @Valid Item itemToBeAdded, BindingResult result) {
-		if(result.hasErrors()) {
-			return "addItem";
-		}
-
- 		try {
-			itemService.save(itemToBeAdded);
-		} catch (Exception up) {
-	      System.out.println("Transaction Failed!!!");
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
+	public void processAddNewItemForm(@RequestBody Item itemToBeAdded) {
+		itemService.save(itemToBeAdded);
  
-		}
-		
-	   	return "redirect:/items";
 	}
-	
-   
 }
